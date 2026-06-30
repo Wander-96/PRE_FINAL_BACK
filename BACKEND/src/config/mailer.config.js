205998f80dce2ba408a72f032746f47d1709a4e1
lake.dev <santiago.lago4@gmail.com>
2026-06-30 19:05:28 -0300

    Fix: Intentar SMTP puerto 587 STARTTLS

--- BACKEND/src/config/mailer.config.js
+++ BACKEND/src/config/mailer.config.js
@@ -6,8 +6,9 @@ Configuración del transportador SMTP para el envío de correos electrónicos.
 */
 const mailer_transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: ENVIRONMENT.GMAIL_USERNAME,
         pass: ENVIRONMENT.GMAIL_PASSWORD,
