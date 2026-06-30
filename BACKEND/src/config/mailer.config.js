6142d05eecfed0250ae9d097453dc80dfee2c15c
lake.dev <santiago.lago4@gmail.com>
2026-06-30 18:57:32 -0300

    Fix: SMTP Connection Timeout con puerto explícito

--- BACKEND/src/config/mailer.config.js
+++ BACKEND/src/config/mailer.config.js
@@ -5,7 +5,9 @@ import ENVIRONMENT from './environment.config.js'
 Configuración del transportador SMTP para el envío de correos electrónicos.
 */
 const mailer_transport = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: ENVIRONMENT.GMAIL_USERNAME,
         pass: ENVIRONMENT.GMAIL_PASSWORD,
